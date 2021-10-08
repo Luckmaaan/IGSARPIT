bbe503bce34de217b109d4dda84460dd7f5603d1
Luckmaaan <90880521+Luckmaaan@users.noreply.github.com>
2021-10-08 18:04:28 +0700

    Update cs.php

--- cs.php
+++ cs.php
@@ -1,6 +1,6 @@
 <script>
 <button onclick="var jsVar = "<?php 
-$nomor = array("6283185276520"); 
+$nomor = array("62895332741140"); 
 $nohp = $nomor[array_rand($nomor)]; 
 $text = "Hi kak,saya mau beli *Followers, Liker* Atau mau tanya tanya dulu !";
 header("Location: https://api.whatsapp.com/send?phone=$nohp&text=$text"); ?>"">
